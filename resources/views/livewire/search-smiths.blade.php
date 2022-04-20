ecf6ac89cd9982e75d3ce3c326ab651a77e91e3e
David <d.holleman700@gmail.com>
2022-04-20 22:20:12 +0200

    Fixed searchbox flex item width

--- resources/views/livewire/search-smiths.blade.php
+++ resources/views/livewire/search-smiths.blade.php
@@ -7,7 +7,7 @@
     @if($searchresults)
     <ul class="appearance-none bg-white border-none rounded text-gray-700 leading-tight focus:outline-none absolute flex flex-row flex-wrap shadow basis-full my-auto w-full lg:w-[50rem]" style="top:462px;">
       @foreach($searchresults as $result)
-          <li class="hover:bg-teal-500 hover:text-white py-4 px-4 border" id="{{ $result->id }}" wire:click="selectOption( '{{$result['name']}}');">
+          <li class="hover:bg-teal-500 hover:text-white py-4 px-4 border flex-1" id="{{ $result->id }}" wire:click="selectOption( '{{$result['name']}}');">
             <p class="uppercase text-xl">#{{ $result['name'] }}</p>
             @for($i = 0; $i < $result['stars']; $i++)
               <x-heroicon-s-star class="w-6 h-6 mr-1 float-left mx-auto my-auto dark:text-black"/>
